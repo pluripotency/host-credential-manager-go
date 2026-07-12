a1d464092cec4d0e35126ffdc15b14b0e35a2b80
Masayoshi Kokuryo <masakoku@gmail.com>
2026-07-12 09:10:30 +0900

    added air

--- build_windows_one_binary.sh
+++ build_windows_one_binary.sh
@@ -1,8 +1,8 @@
 #! /bin/bash
 # 1. フロントエンドのビルド
-cd frontend && npm run build && cd ..
+cd front && npm run build && cd ..
 
 # 2. Windows向けにクロスコンパイル (環境変数を指定)
-SET GOOS=windows
-SET GOARCH=amd64
+export GOOS=windows
+export GOARCH=amd64
 go build -o myapp.exe main.go
